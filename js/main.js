814f1c459affce4a00da32f3c494fffd66ef1e4d
pijeomah <promiseij4919@gmail.com>
2023-01-27 16:35:41 +0100

    wheel of time client first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const charName = document.querySelector('input').value
+    try {
+        const response = await fetch(`https://nice-tan-crayfish-veil.cyclic.app/api/${charName}`)
+        const data = await response.json()
+        console.log(data)
+
+        document.getElementById('charName').innerText = data.characterName
+        document.getElementById('charBirthplace').innerText = data.birthPlace
+        document.getElementById('charNation').innerText = data.nationality
+        document.getElementById('charFeatures').innerText = data.description
+        document.getElementById('charAbilities').innerText = data.abilities
+        document.getElementById('charTitles').innerText = data.titles 
+
+        document.getElementById('charImage').src = data.image
+        document.getElementById('charCaption').innerText = data.characterName
+
+        
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
